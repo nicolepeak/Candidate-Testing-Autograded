e759ed535aa05d806d9a73e2a6daa00e013522ba
Nicole Peak <colecpeak@gmail.com>
2024-01-13 20:46:10 -0600

    part one complete with comments

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,12 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName = ""; //I defined the variable as empty using "" 
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
+let question = "Who was the first American woman in space? "; // I defined all of these variables for 1st Q
+let correctAnswer = "Sally Ride"; 
+let candidateAnswer = "";   // initialized as empty string
 
 
 //TODO: Variables for Part 2
@@ -19,22 +20,22 @@ let candidateAnswers;
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("What is your name?")
-}
+}           // I used readline-sync module to prompt user for their name
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 candidateAnswer = input.question (question)
 
-}
-
+}    // using readline-sync and my defined question variable, I prompted a response from user and stored their input
+    
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === "Sally Ride") {
+if (candidateAnswer == "Sally Ride") {
   console.log("Correct Answer!")
 } else {
   console.log ("Incorrect Answer")
-}
+} //I used an if else function to print if the user had the correct answer or not
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -46,7 +47,7 @@ if (candidateAnswer === "Sally Ride") {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello,", candidateName , "!");
+   console.log("Hello,", candidateName , "!");  // I printed the user's name using the variable associated with the user input
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
