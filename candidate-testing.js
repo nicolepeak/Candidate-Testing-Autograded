a3392bb6d8b7adcadfbb7ee552c749d2d12410fc
Nicole Peak <colecpeak@gmail.com>
2024-01-18 16:43:52 -0600

    part 2 complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,29 +12,43 @@ let candidateAnswer = "";   // initialized as empty string
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers= [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = "";
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name?")
+candidateName = input.question("What is your name? ")
 }           // I used readline-sync module to prompt user for their name
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question (question)
-
-}    // using readline-sync and my defined question variable, I prompted a response from user and stored their input
+for (i = 0; candidateAnswers=true; i++){
+candidateAnswers = input.question (questions[i])
+if (i>3) {break};
+}
+}    
     
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == "Sally Ride") {
-  console.log("Correct Answer!")
-} else {
-  console.log ("Incorrect Answer")
+  if (candidateAnswer == "Sally Ride") {
+    console.log("Correct Answer!")
+  } else {
+    console.log ("Incorrect Answer")
 } //I used an if else function to print if the user had the correct answer or not
 
 
